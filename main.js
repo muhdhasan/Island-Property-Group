a63740ab586c36e0a9d00874b92551bd6d405da6
Lmao5 <cilipadi270@gmail.com>
2021-05-17 14:37:43 +0800

    Added some fields

---
+++ main.js
@@ -0,0 +1,9 @@
+(function prediceHousePrice(){
+    'use strict'
+var housingEstate = document.getElementById('housingEstate').value;
+
+// document.getElementById('predictedHousePrice').innerHTML = "Testing"
+
+})
+
+document.getElementById('predictedHousePrice').innerHTML = "Testing"
