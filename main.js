4cd9cfd11f7a299c55fe79b7ff9ad1446a291a9a
Lmao5 <cilipadi270@gmail.com>
2021-05-18 01:20:55 +0800

    Testing JavaScript Code

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-(function prediceHousePrice(){
-    'use strict'
-var housingEstate = document.getElementById('housingEstate').value;
+"use strict";
+function prediceHousePrice() {
+  var housingEstate = document.getElementById("housingEstateSelect").value;
 
-// document.getElementById('predictedHousePrice').innerHTML = "Testing"
+  console.log(housingEstate);
+  document.getElementById("predictedHousePrice").innerHTML = "Testing";
+  var currentdate = new Date();
+  console.log(currentdate);
 
-})
+  var leaseDate = document.getElementById("leaseDateStart").value;
+  console.log("Lease Date: " + leaseDate);
+  console.log(typeof leaseDate);
+  var dateDiff = currentdate - leaseDate;
+  console.log("Date Diff: " + dateDiff);
 
-document.getElementById('predictedHousePrice').innerHTML = "Testing"
+  //if(){}
+  //else if
+  //else
+  document
+    .getElementById("submitBtn")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+
+  return (document.getElementById("predictedHousePrice").innerHTML = "Testing");
+}
