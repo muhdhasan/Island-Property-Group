6601b4e56343241a1c50738195ecbcb5bcba8775
Lmao5 <cilipadi270@gmail.com>
2021-05-18 17:32:49 +0800

    Added JS Validation

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 "use strict";
-function prediceHousePrice() {
-  var housingEstate = document.getElementById("housingEstateSelect").value;
 
-  console.log(housingEstate);
-  document.getElementById("predictedHousePrice").innerHTML = "Testing";
+// Validating user inputs
+var validateInput = (housingEstate, housingType, floorSizeSqm, flatType) => {
+  if (typeof housingEstate === undefined || housingEstate.length <= 0 || housingEstate === null) {
+    console.log("Undefined input for housing estate field.");
+    return false;
+  }
+  else if(typeof housingType === undefined || housingType.length <= 0 || housingType === null){
+    console.log("Undefined input for HDB type field.");
+    return false;
+  }
+  else if(typeof floorSizeSqm === undefined || floorSizeSqm.length <= 0 || floorSizeSqm === null){
+    console.log("Invalid HDB Floor Size");
+    return false;
+  }
+  else if(typeof flatType === undefined || flatType.length <= 0 || flatType === null){
+    console.log("Invalid Flat Type");
+    return false;
+  }
+  else{
+    console.log("All fields are valid.")
+    return true;
+  }
+};
+
+function prediceHousePrice() {
+
+  // Retrieving values from user input
+  var housingEstate = document.getElementById("housingEstateSelect").value;
+  var housingType = document.getElementById("housingTypeSelect").value;
+  var floorSizeSqm = document.getElementById("floorArea").value;
+  var flatType = document.getElementById("flatModel").value;
+  var leaseStart = document.getElementById("leaseDateStart").value;
+  var resaleDate = document.getElementById("resaleDate").value;
+
+  var validateInputResult = validateInput(housingEstate, housingType, floorSizeSqm, flatType);
+  console.log("Valid Input: " + validateInputResult);
+
+
   var currentdate = new Date();
   console.log(currentdate);
 
@@ -13,14 +47,21 @@ function prediceHousePrice() {
   var dateDiff = currentdate - leaseDate;
   console.log("Date Diff: " + dateDiff);
 
-  //if(){}
-  //else if
-  //else
-  document
-    .getElementById("submitBtn")
-    .addEventListener("click", function (event) {
-      event.preventDefault();
-    });
+  // document
+  //   .getElementById("submitBtn")
+  //   .addEventListener("click", function (event) {
+  //     event.preventDefault();
+  //   });
 
-  return (document.getElementById("predictedHousePrice").innerHTML = "Testing");
+  // Conditions
+  if (validateInputResult === false) {
+    console.log("Validation Failed");
+    return;
+  } else if (validateInputResult === undefined) {
+    console.log("Validation Failed");
+    return;
+  } else {
+    return (document.getElementById("predictedHousePrice").innerHTML =
+      "Testing");
+  }
 }
