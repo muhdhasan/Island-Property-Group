6cba3bddd581ceb35b6ab593971807407317bd4b
Lmao5 <cilipadi270@gmail.com>
2021-06-13 20:52:33 +0800

    Added some EDA codes

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
+require('dotenv').config();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  // res.render('index');
+  res.send("<h1>Hello World</h1>")
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const port = process.env.port || 5000
+
+app.listen(port, () => {
+  console.log(`Server started ad ${port}`);
 });
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// // const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
