8b67e2ff0826bd2e06a5f74a8cad251827ff6a58
Lmao5 <cilipadi270@gmail.com>
2021-06-26 15:25:44 +0800

    Add error page

--- app.js
+++ app.js
@@ -23,15 +23,48 @@ app.use('/', mainRoute)
 app.use('/user', userRoute)
 app.use('/property', propertyRoute)
 
+// Error Codes
+app.use((req, res) => {
+  if (res.status(400)) {
+    res.render('errorCodes', {
+      errorCode: '400',
+      errorMessage: 'Its a bad request!'
+    })
+  } else if (res.status(404)) {
+    res.render('errorCodes', {
+      errorCode: '404',
+      errorMessage: 'Are you on the right page?'
+    })
+  } else if (res.status(500)) {
+    res.render('errorCodes', {
+      errorCode: '500',
+      errorMessage: 'Its a internal server error!'
+    })
+  } else if (res.status(502)) {
+    res.render('errorCodes', {
+      errorCode: '502',
+      errorMessage: 'Its a bad gateway!'
+    })
+  } else if (res.status(503)) {
+    res.render('errorCodes', {
+      errorCode: '503',
+      errorMessage: 'Service is currently unavailable.'
+    })
+  }
+})
+
 // Port number defaults to 5000 if env file is not available
 const port = process.env.port || 5000
 
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}
+
 // Create HTTP Server
-https.createServer({
-     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-   },
-   app
+https.createServer(
+  options,
+  app
 ).listen(port, () => {
   console.log(`Server started ad ${port}`)
 })
