3963eac57cc66f05ea4994f26fc62d3c05c29723
Lmao5 <cilipadi270@gmail.com>
2021-07-05 22:02:25 +0800

    Added template for CRUD Property

--- routes/property.js
+++ routes/property.js
@@ -16,35 +16,38 @@ router.get('/propertylist', (req, res) => {
   res.render('property/property-grid', { title: title })
 })
 
-// router.get('/createPropertyResaleListing', (req, res) => {
-//   const title = 'List of Properties'
-//   res.render('property/property-grid', { title: title })
-// })
+
+router.get('/createPublicResaleListing', (req, res) => {
+  const title = 'Create HDB Resale Listing'
+  res.render('property/createPublicResale', { title: title })
+})
 
 // Fixed data for testing
 router.post('/createPublicResaleListing', (req, res) => {
 
+  // Inputs
   // let 
   // let address = req.body.address
   // let description = req.body.description
   // let resalePrice = // Call predict function here
-  // let town = req.body.town
-  // let flatType = req.body.flatType
-  // let flatModel = req.body.flatModel
+  let town = req.body.town
+  let flatType = req.body.flatType
+  let flatModel = req.body.flatModel
   // let flatLevel = req.body.flatLevel // Call function to choose storey range according to user input
   // let floorSqm = req.body.floorSqm
   // let leaseStartDate = moment(req.body.leaseCommenceDate,'DD/MM/YYYY')
   // let dateOfSale = moment(req.body.resaleDate,'DD/MM/YYYY')
 
+  // Will add input validation here later
 
   hdbResale.create({
     id: uuid.v4(),
     address: "Sample Address",
     description: "Sample Description",
     resalePrice: 500000,
-    town: "ANG MO KIO",
-    flatType: "5 Room",
-    flatModel: "IMPROVED",
+    town: town,
+    flatType: flatType,
+    flatModel: flatModel,
     flatLevel: "5",
     floorSqm: 90,
     // Issue with dates
