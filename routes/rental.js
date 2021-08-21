49e80f1b19f231540caacf7a0fec742c230ba8be
Lmao5 <cilipadi270@gmail.com>
2021-08-21 20:14:30 +0800

    Replaced rental route with latest changes

--- routes/rental.js
+++ routes/rental.js
@@ -6,23 +6,25 @@ const privateRental = require('../models/PrivateRental')
 
 // Required node modules
 const uuid = require('uuid')
+const moment = require('moment')
 const fetch = require('node-fetch')
 
-const baseAPIUrl = 'http://localhost:8000/api/'
+const baseAPIUrl = process.env.baseAPIUrl || 'http://localhost:8000/api/'
 const floorRangeSelector = require('../helpers/floorRangeSelector')
 const PrivateRental = require('../models/PrivateRental')
 
 // Consolidate check regex for uuid
 const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
 
-// Call predict resale API
-async function predictHouseRent (postal_district, type, bedrooms, floorSqF, leaseDate) {
+// Call predict rental API
+async function predictHouseRent(postal_district, type, bedrooms, floorSqF, leaseDate) {
+
   const body = {
     Postal_District: postal_district,
     Type: type,
     No_Bedroom: bedrooms,
     Floor_Area: floorSqF,
-    Lease_Commencement_Date: leaseDate
+    Lease_Commencement_Date: leaseDate,
 
   }
   return new Promise((result, err) => {
@@ -41,8 +43,7 @@ async function predictHouseRent (postal_district, type, bedrooms, floorSqF, leas
       })
   })
 }
-
-// HDB Properties that are currently viewable to customers can be found here
+// Rental Properties that are viewable to customers
 router.get('/base', (req, res) => {
   const title = 'Rental Properties'
   privateRental.findAll({
@@ -51,19 +52,21 @@ router.get('/base', (req, res) => {
       isViewable: true
     },
     raw: true
+
   }).then((privateRental) => {
     res.render('rental/base', { title, privateRental: privateRental })
   })
 })
-// View individual HDB Resale Page
+
+// View each Rental Properties Page
 router.get('/rentalListing/:id', (req, res) => {
   const title = 'Rental Properties'
   const secondaryTitle = '304 Blaster Up'
 
-  // Refer to mysql workbench for all property id
+  // Store ID in sql database
   const rentID = req.params.id
 
-  // Redirect to homepage if uuid is invalid
+  // Redirect to homepage 
   if (uuidRegex.test(rentID) === false) {
     res.redirect('/')
   } else {
@@ -74,7 +77,7 @@ router.get('/rentalListing/:id', (req, res) => {
           isViewable: true
         }
       })
-    // Will display more information regarding this property later
+      // Display information
       .then((PrivateRental) => {
         const rentalPrice = PrivateRental.monthlyRent
         const address = PrivateRental.address
@@ -83,9 +86,13 @@ router.get('/rentalListing/:id', (req, res) => {
         const floorSqm = PrivateRental.floorSqm
         const leaseCommenceDate = PrivateRental.leaseCommenceDate
         const description = PrivateRental.description
+        const amenities = PrivateRental.amenities
+        const postalDistrict = PrivateRental.postalDistrict
 
+        const annualprice = Math.round(rentalPrice * 12).toFixed(2)
+        const downpayment = Math.round(rentalPrice * 3).toFixed(2)
+        const priceperSqm = rentalPrice/floorSqm
         res.render('rental/rentalListing', {
-          rentID,
           address,
           title,
           secondaryTitle,
@@ -93,8 +100,14 @@ router.get('/rentalListing/:id', (req, res) => {
           houseType,
           numberOfBedroom,
           leaseCommenceDate,
+          amenities,
           floorSqm,
-          description
+          description,
+          postalDistrict,
+          rentID,
+          annualprice,
+          downpayment,
+          priceperSqm
         })
       })
       .catch((err) => {
@@ -106,7 +119,7 @@ router.get('/createRental', (req, res) => {
   const title = 'Rental Properties'
   res.render('rental/createRental', { title: title })
 })
-// Fixed data for testing
+
 router.post('/createRental', (req, res) => {
   const title = 'Rental Properties'
   const filterSpecialRegex = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/
@@ -114,48 +127,30 @@ router.post('/createRental', (req, res) => {
   const RentId = uuid.v4()
   const address = req.body.address1
   const description = req.body.description
-  // Will add input validation here later
   const postal_district = req.body.postal_district
-  // console.log(postal_district)
   const type = req.body.Type
   const bedrooms = req.body.No_Bedroom
-
-  // Call floor range selector to select floor range from floor level accordingly
   const floorSqF = req.body.floorFt
-
-  // Date related inputs
   const leaseDate = req.body.Lease_Commencement_Date
+  const amenities = req.body.amenities
 
-  // Input Validation
-  // if (filterSpecialRegex.test(address) === false) {
-  //   return console.log('Address contains special characters')
-  // }
-  // // if (filterSpecialRegex.test(description) === false) {
-  // //   return console.log('Description contains special characters')
-  // // }
-
-  // // Check if resale date is at least 5 years from lease commence date
-  // const totalMilisecondsPerDay = 1000 * 60 * 60 * 24
-  // const yearDiff = ((dateOfSale - leaseStartDate) / totalMilisecondsPerDay) / 365
-  // if (yearDiff < 5) {
-  //   return console.log('Ensure that resale date is at least 5 years from lease date')
-  // }
-
-  // // Call predicting api for public housing
+  // Call predicting api for rental
   const rentValue = predictHouseRent(postal_district, type, bedrooms, floorSqF, leaseDate)
   rentValue.then((response) => {
-    console.log('postal_district: ', postal_district)
-    console.log('type: ', type)
-    console.log('bedrooms: ', bedrooms)
-    console.log('floorSqF: ', floorSqF)
-    console.log('leaseDate: ', leaseDate)
-    console.log('Resale Value', rentValue)
-    const description = 'Sample Description'
+    // console.log('postal_district: ', postal_district)
+    // console.log('type: ', type)
+    // console.log('bedrooms: ', bedrooms)
+    // console.log('floorSqF: ', floorSqF)
+    // console.log('leaseDate: ', leaseDate)
+    // console.log('Resale Value', rentValue)
+    // const description = 'Sample Description'
+
     PrivateRental
       .create({
         id: RentId,
         address: address,
         description: description,
+        amenities: amenities,
         monthlyRent: Math.round(response),
         houseType: type,
         numberOfBedroom: bedrooms,
@@ -165,13 +160,101 @@ router.post('/createRental', (req, res) => {
         isViewable: true
       })
       .then((result) => {
-        console.log('Testing')
-        // Redirect to confirming property page
-        // res.redirect('/rental/createListing' + RentId)
+        console.log('Returning to homepage')
         res.redirect('/rental/base')
       })
       .catch((err) => console.log('Error: ' + err))
   })
 })
+// Edit Function for rental listings
+router.get('/editRentalListing/:id', (req, res) => {
+  const title = 'Edit Rental Listing'
 
+  const rentID = req.params.id
+  // Find rental property by id
+  PrivateRental.findOne({
+    where: { id: rentID }
+  }).then((PrivateRental) => {
+    // Getting result from database
+    const rentalPrice = PrivateRental.monthlyRent
+    const address = PrivateRental.address
+    const houseType = PrivateRental.houseType
+    const numberOfBedroom = PrivateRental.numberOfBedroom
+    const floorSqm = PrivateRental.floorSqm
+    const leaseCommenceDate = PrivateRental.leaseCommenceDate
+    const postalDistrict = PrivateRental.postal_district
+    const description = PrivateRental.description
+    const amenities = PrivateRental.amenities
+
+    res.render('rental/editRentalListing', {
+      address,
+      title,
+      rentalPrice,
+      houseType,
+      numberOfBedroom,
+      leaseCommenceDate,
+      postalDistrict,
+      floorSqm,
+      description,
+      amenities,
+      rentID
+    })
+  }).catch((err) => console.log('Error: ', err))
+})
+// Update rental property information to database
+router.put('/editRentalListing/:id', (req, res) => {
+
+  const rentID = req.params.id
+
+  const filterSpecialRegex = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/
+
+  // Inputs
+  const address = req.body.address
+  const postal_district = req.body.postal_district
+  const description = req.body.description
+  const bedrooms = req.body.No_Bedroom
+  const type = req.body.Type
+  const floorSqF = req.body.floorFt
+  const leaseDate = req.body.leaseCommenceDate
+  const amenities = req.body.amenities
+
+  // Call predicting api for rental property
+  const rentValue = predictHouseRent(postal_district, type, bedrooms, floorSqF, leaseDate)
+  rentValue.then((response) => {
+    // console.log('postal_district: ', postal_district)
+    // console.log('type: ', type)
+    // console.log('bedrooms: ', bedrooms)
+    // console.log('floorSqF: ', floorSqF)
+    // console.log('leaseDate: ', leaseDate)
+    // console.log('Resale Value', rentValue)
+    // const description = 'Sample Description'
+    // Update rental listing according to id
+    PrivateRental.update({
+      address: address,
+      description: description,
+      amenities: amenities,
+      monthlyRent: Math.round(response),
+      houseType: type,
+      numberOfBedroom: bedrooms,
+      postalDistrict: postal_district,
+      floorSqm: floorSqF,
+      leaseCommenceDate: leaseDate,
+    }, {
+      where: { id: rentID }
+    }).then(() => {
+      res.redirect('/rental/base')
+    }).catch((err) => { console.log('Error in updating Rental Listing: ', err) })
+  })
+})
+
+// Delete rental listing
+router.get('/deleteRentalListing/:id', (req, res) => {
+  const rentID = req.params.id
+  PrivateRental.destroy({
+    where: { id: rentID }
+  }).then(() => {
+    console.log('Deleted rental listing')
+    res.redirect('/rental/base')
+  }).catch((err) => { console.log('Error: ', err) })
+})
 module.exports = router
